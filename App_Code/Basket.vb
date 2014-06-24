53ee986b30967b675f1f2a7490da0915feca1877
ThePCCure <shanecox@thepccure.co.uk>
2014-06-24 11:50:36 +0100

    Building Functionality and Coding standard changed
    .

--- App_Code/Basket.vb
+++ App_Code/Basket.vb
@@ -2,31 +2,35 @@
 
 Public Class Basket
 #Region "Propities"
-    Private m_Product_Basket_Items() As Product
+    Private mProduct_Basket_Items() As Product
     Public Property Product_Basket_Items As Product()
         Get
-            Return m_Product_Basket_Items
+            Return mProduct_Basket_Items
         End Get
         Set(ByVal value As Product())
-            m_Product_Basket_Items = value
+            mProduct_Basket_Items = value
         End Set
     End Property
 #End Region
     Private Sub New()
     End Sub
 
-    Private Sub Product_Basket_AddItem(l_Product_Basket_Item As Product)
-        If ((m_Product_Basket_Items.Length - 1) > 0) Then
-            m_Product_Basket_Items(m_Product_Basket_Items.Length - 1) = l_Product_Basket_Item
+    Private Sub Product_Basket_AddItem(lProduct_Basket_Item As Product)
+        If ((mProduct_Basket_Items.Length - 1) > 0) Then
+            mProduct_Basket_Items(mProduct_Basket_Items.Length - 1) = lProduct_Basket_Item
         Else
-            m_Product_Basket_Items(0) = l_Product_Basket_Item
+            mProduct_Basket_Items(0) = lProduct_Basket_Item
         End If
     End Sub
 
     Public Function basketValue() As Double
-        Dim l_dbl_Basket_value As Double
+        Dim l_dbl_Basket_value As Double = 0
 
-        If (Not (IsNothing(m_Product_Basket_Items))) Then
+        If (Not (IsNothing(mProduct_Basket_Items))) Then
+
+            For Each item As Product In mProduct_Basket_Items
+                l_dbl_Basket_value += item.dbl_Product_Price
+            Next
             Return l_dbl_Basket_value
         Else
             Return Nothing
--- App_Code/Product.vb
+++ App_Code/Product.vb
@@ -2,56 +2,74 @@
 
 Public Class Product
 #Region "Properties"
-    Private m_str_Product_Name As String
+    Private mstr_Product_Name As String
     Public Property str_Product_Name() As String
         Get
-            Return m_str_Product_Name
+            Return mstr_Product_Name
         End Get
         Set(ByVal value As String)
-            m_str_Product_Name = value
+            mstr_Product_Name = value
         End Set
     End Property
 
-    Private m_str_Product_Manufacturer As String
+    Private mstr_Product_Manufacturer As String
     Public Property str_Product_Manufacturer() As String
         Get
-            Return m_str_Product_Manufacturer
+            Return mstr_Product_Manufacturer
         End Get
         Set(ByVal value As String)
-            m_str_Product_Manufacturer = value
+            mstr_Product_Manufacturer = value
         End Set
     End Property
 
-    Private m_str_Product_Discription As String
+    Private mstr_Product_Discription As String
     Public Property str_Product_Discription() As String
         Get
-            Return m_str_Product_Discription
+            Return mstr_Product_Discription
         End Get
         Set(ByVal value As String)
-            m_str_Product_Discription = value
+            mstr_Product_Discription = value
         End Set
     End Property
 
-    Private m_dbl_Product_Price As Double
+    Private mdbl_Product_Price As Double
     Public Property dbl_Product_Price() As Double
         Get
-            Return m_dbl_Product_Price
+            Return mdbl_Product_Price
         End Get
         Set(ByVal value As Double)
-            m_dbl_Product_Price = value
+            mdbl_Product_Price = value
         End Set
     End Property
 
-    Private m_dbl_Product_PostalCost As String
+    Private mdbl_Product_PostalCost As String
     Public Property dbl_Product_PostalCost() As String
         Get
-            Return m_dbl_Product_PostalCost
+            Return mdbl_Product_PostalCost
         End Get
         Set(ByVal value As String)
-            m_dbl_Product_PostalCost = value
+            mdbl_Product_PostalCost = value
         End Set
     End Property
+
+    'NEED TO THINK ABOUT A WAY TO HAVE PRODUCT CATEGORIES, PROBABLY NEED TO PULL THIS FROM A DATABASE TABLE
+    '
+    'Private m As String
+    'Public Property NewProperty() As String
+    '    Get
+    '        Return newPropertyValue
+    '    End Get
+    '    Set(ByVal value As String)
+    '        newPropertyValue = value
+    '    End Set
+    'End Property
+
 #End Region
-
-
+    Sub New(ByVal lstr_Product_Name As String, ByVal lstr_Product_Manufacturer As String, ByVal lstr_Product_Discription As String, ByVal ldbl_Product_Price As Double, ldbl_Product_PostalCost As Double)
+        mstr_Product_Name = lstr_Product_Name
+        mstr_Product_Manufacturer = lstr_Product_Manufacturer
+        mstr_Product_Discription = lstr_Product_Discription
+        mdbl_Product_Price = ldbl_Product_Price
+        mdbl_Product_PostalCost = ldbl_Product_PostalCost
+    End Sub
 End Class
