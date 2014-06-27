15d3584202ba683a73601ef6fe310132785b692e
ThePCCure <shanecox@thepccure.co.uk>
2014-06-27 16:42:44 +0100

    SQL connection string
    will need to move connection string in config file

--- App_Code/db.vb
+++ App_Code/db.vb
@@ -1,5 +1,20 @@
 ﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Data
+Imports System.Data.SqlClient
+
+
+'Server=.\SQLExpress;AttachDbFilename=C:\MyFolder\MyDataFile.mdf;Database=dbname;Trusted_Connection=Yes;
+
 
 Public Class db
-    
+    Public Sub New()
+        Dim sConnectionString As String = "Server=OFFICEPC\SQLExpress; Trusted_Connection=True"
+        Dim objConn As New SqlConnection(sConnectionString)
+        objConn.Open()
+        Dim sSQL As String = "DELETE FROM [ThePCCureeCommerce].[dbo].[Product_Type] WHERE [Product_ID] = 8"
+        Dim objCmd As New SqlCommand(sSQL, objConn)
+        objCmd.ExecuteNonQuery()
+    End Sub
+
 End Class
--- Shop.aspx.vb
+++ Shop.aspx.vb
@@ -2,4 +2,8 @@
 Partial Class Shop
     Inherits System.Web.UI.Page
 
+    Public Sub New()
+        Dim product_Types As New db
+    End Sub
+
 End Class
