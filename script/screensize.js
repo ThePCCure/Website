74856289fa704da5f3b2c2c1a42f9b863b8561c1
ThePCCure <shanecox@thepccure.co.uk>
2014-07-01 19:25:10 +0100

    Moving main resize to outside function
    need to call the rezise on page load

--- script/screensize.js
+++ script/screensize.js
@@ -1,29 +1,32 @@
 ﻿//document.body.clientWidth
 //screen.width 
 
-function pagesizeinit(){    
+function pagesizeinit() {
     $(window).resize(function () {
         //$("#screenwidth").text($(window).width());
-
-        if ($(window).width() < 1200) {
-            $("#fb").css("display", "none");
-        }
-        else if ($(window).width() < 1320) {
-            $("#content").css("width", "900");
-            $("#content").css("margin-left", "0");
-            $("#fb").css("display", "block");
-        } 
-        else if ($(window).width() < 1625) {
-            $("#content").css("width", "1000");
-            $("#content").css("margin-left", "0");
-            $("#fb").css("display", "block");
-        }
-        else {
-            $("#content").css("margin-left", "auto");
-            $("#content").css("width", "1000");
-            $("#fb").css("display", "block");
-        }
+        resizepages()
     })
+}
+
+
+function resizepages() {
+    if ($(window).width() < 1200) {
+        $("#fb").css("display", "none");
+    }
+    else if ($(window).width() < 1320) {
+        $("#content").css("width", "900");
+        $("#content").css("margin-left", "0");
+        $("#fb").css("display", "block");
+    }
+    else if ($(window).width() < 1625) {
+        $("#content").css("width", "1000");
+        $("#content").css("margin-left", "0");
+        $("#fb").css("display", "block");
+    }
+    else {
+        $("#content").css("margin-left", "auto");
+        $("#content").css("width", "1000");
+        $("#fb").css("display", "block");
     }
 
-
+}
