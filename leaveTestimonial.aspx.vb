bfec0d0dccc39beca9f8415eece30135cdfa40a2
ThePCCure <shanecox@thepccure.co.uk>
2014-07-13 16:10:43 +0100

    testimonials near done
    Currently posting to feedback page

--- leaveTestimonial.aspx.vb
+++ leaveTestimonial.aspx.vb
@@ -1,5 +1,6 @@
-﻿Imports System.Xml
+﻿Imports System
 Imports System.IO
+Imports System.Xml
 
 Partial Class leaveTestimonial
     Inherits System.Web.UI.Page
@@ -9,24 +10,31 @@ Partial Class leaveTestimonial
     End Sub
 
     Protected Sub SubmitReview_Click(sender As Object, e As EventArgs) Handles SubmitReview.Click
-        '' need all these varibles
-        Dim name As String = txt_name.Value
-        Dim location As String = txt_location.Value
-        Dim workType As String = txt_worktype.Value
-        Dim message As String = txt_message.Value
-
-
-        Dim myXmlDocument As XmlDocument = New XmlDocument()
-        Dim xmllocation As String = "F:\Users\Shane\Code\WebVersionControl\testimonials.xml"
-        myXmlDocument.Load(xmllocation)
-
-        Dim node As XmlNode
-        node = myXmlDocument.DocumentElement
-
-        Dim Root As XmlElement = node.OwnerDocument.DocumentElement
-
-        'Root.AppendChild()
-
+        Dim myXmlDocument As New XmlDocument()
+        Dim xmlDocLocation As String = "F:\Users\Shane\Code\WebVersionControl\testimonials.xml"
+        myXmlDocument.Load(xmlDocLocation)
+        addNode("Feedback", Nothing, Nothing, myXmlDocument)
+        addNode("message", txt_message.Value, "Feedback", myXmlDocument)
+        addNode("worktype", txt_worktype.Value, "Feedback", myXmlDocument)
+        addNode("date", Date.Today.Date, "Feedback", myXmlDocument)
+        addNode("location", txt_location.Value, "Feedback", myXmlDocument)
+        addNode("name", txt_name.Value, "Feedback", myXmlDocument)
+        myXmlDocument.Save(xmlDocLocation)
 
     End Sub
+
+    Private Sub addNode(ByRef NodeName As String, ByVal NodeText As String, ByVal parentNodeName As String, ByRef xmlDoc As XmlDocument)
+        Dim elem As XmlElement = xmlDoc.CreateElement(NodeName)
+        elem.InnerText = NodeText
+        'Add the node to the document.
+        If IsNothing(parentNodeName) Then
+            Dim root As XmlNode = xmlDoc.DocumentElement()
+            root.PrependChild(elem)
+        Else
+            Dim root As XmlNode = xmlDoc.DocumentElement(parentNodeName)
+            root.PrependChild(elem)
+        End If
+    End Sub
 End Class
+
+
